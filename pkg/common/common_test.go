8c17c6029d3e9029e026bc7998b71f75aa7dd532
Arthur Pitman <arthur.pitman@dynatrace.com>
2021-06-10 16:20:22 +0200

    Add basic unit tests
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

---
+++ pkg/common/common_test.go
@@ -0,0 +1,62 @@
+package common
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_parseDynatraceConfigFile(t *testing.T) {
+	tests := []struct {
+		name       string
+		yamlString string
+		want       DynatraceConfigFile
+		wantErr    bool
+	}{
+		{
+			name:       "empty string",
+			yamlString: "",
+			want:       DynatraceConfigFile{},
+			wantErr:    false,
+		},
+		{
+			name: "valid yaml no dashboard",
+			yamlString: `spec_version: '0.1.0'
+dtCreds: dyna`,
+			want: DynatraceConfigFile{
+				SpecVersion: "0.1.0",
+				DtCreds:     "dyna"},
+			wantErr: false,
+		},
+		{
+			name: "valid yaml with dashboard",
+			yamlString: `spec_version: '0.1.0'
+dtCreds: dyna
+dashboard: dash`,
+			want: DynatraceConfigFile{
+				SpecVersion: "0.1.0",
+				DtCreds:     "dyna",
+				Dashboard:   "dash"},
+			wantErr: false,
+		},
+		{
+			name: "invalid yaml",
+			yamlString: `spec_version: '0.1.0'
+dtCreds: dyna,
+dashboard: ****`,
+			want:    DynatraceConfigFile{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseDynatraceConfigFile(tt.yamlString)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseDynatraceConfigFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseDynatraceConfigFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
